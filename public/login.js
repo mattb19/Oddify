6d7cd83b597ffe73669d5c2ced803e9802cddda9
mattb19 <matthew51672@gmail.com>
2024-09-22 19:23:01 -0400

    update

--- public/login.js
+++ public/login.js
@@ -1,49 +1,18 @@
-// const loginElement = document.getElementById("loginForm");
-// const registerElement = document.getElementById("registerForm");
-// const signUpElement = document.getElementById("signUp");
-// const signInElement = document.getElementById("signIn");
-// console.log(registerElement);
-// function login() {
-//     loginElement.style.animation = "in 5s forwards";
-//     registerElement.style.animation = "out 5s forwards";
-//     signUpElement.style.animation = "in 4s forwards";
-//     signInElement.style.animation = "out 4s forwards";
-// }
-// function register() {
-//     registerElement.style.animation = "in 5s forwards";
-//     loginElement.style.animation = "out 5s forwards";
-//     signInElement.style.animation = "in 4s forwards";
-//     signUpElement.style.animation = "out 4s forwards";
-// }
-document.addEventListener("DOMContentLoaded", function () {
-    const loginElement = document.getElementById("loginForm");
-    const registerElement = document.getElementById("registerForm");
-    const signUpElement = document.getElementById("signUp");
-    const signInElement = document.getElementById("signIn");
-
-    console.log(registerElement);
-});
-
+const loginElement = document.getElementById("loginForm");
+const registerElement = document.getElementById("registerForm");
+const signUpElement = document.getElementById("signUp");
+const signInElement = document.getElementById("signIn");
+console.log(registerElement);
+console.log("JS Has been reached")
 function login() {
-    const loginElement = document.getElementById("loginForm");
-    const registerElement = document.getElementById("registerForm");
-    const signUpElement = document.getElementById("signUp");
-    const signInElement = document.getElementById("signIn");
-
-    loginElement.style.animation = "in 5s forwards";
-    registerElement.style.animation = "out 5s forwards";
-    signUpElement.style.animation = "in 4s forwards";
-    signInElement.style.animation = "out 4s forwards";
+    loginElement.style.animation = "in 0.1s forwards";
+    registerElement.style.animation = "out 0.1s forwards";
+    signUpElement.style.animation = "in 0.1s forwards";
+    signInElement.style.animation = "out 0.1s forwards";
 }
-
 function register() {
-    const loginElement = document.getElementById("loginForm");
-    const registerElement = document.getElementById("registerForm");
-    const signUpElement = document.getElementById("signUp");
-    const signInElement = document.getElementById("signIn");
-
-    registerElement.style.animation = "in 5s forwards";
-    loginElement.style.animation = "out 5s forwards";
-    signInElement.style.animation = "in 4s forwards";
-    signUpElement.style.animation = "out 4s forwards";
+    registerElement.style.animation = "in 0.1s forwards";
+    loginElement.style.animation = "out 0.1s forwards";
+    signInElement.style.animation = "in 0.1s forwards";
+    signUpElement.style.animation = "out 0.1s forwards";
 }
